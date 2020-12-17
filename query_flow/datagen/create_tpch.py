3e2e3fce324c15fad147dec9e856d7f9c28a76e2
etrabelsi <etrabelsi@salesforce.com>
2020-12-17 13:07:33 +0200

    connection string for datagen

--- query_flow/datagen/create_tpch.py
+++ query_flow/datagen/create_tpch.py
@@ -3,9 +3,8 @@ from sqlalchemy import create_engine
 from tqdm import tqdm
 
 
-def load_tpch():
-    # TODO
-    with create_engine('postgresql:///etrabelsi_thesis').connect() as con:
+def load_tpch(conn_str):
+    with create_engine(conn_str).connect() as con:
         for cmd_file in tqdm(["tpch-load.sql", "tpch-pkeys.sql", "tpch-alter.sql",  "tpch-index.sql"]):
             print(f"Executing {cmd_file}")
             with open(f'datagen/tpch_assets/dss/{cmd_file}') as query_file:
@@ -13,4 +12,9 @@ def load_tpch():
 
 
 if __name__ == "__main__":
-    load_tpch()
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Create TPC-H dataset.')
+    parser.add_argument('conn_str', type=str, help='SQLAlchemy connection string.')
+    args = parser.parse_args()
+    load_tpch(args.conn_str)
